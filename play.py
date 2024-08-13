9a84e9c2a2eea5ceacced5449ef0cfcaf48d8030
DipSudo <sudipsun97@gmail.com>
2024-08-13 01:08:44 +0100

    New code for api

--- play.py
+++ play.py
@@ -1,13 +1,16 @@
 import requests
 import spotipy
-
+from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
+import os 
 
-from info import *
+load_dotenv()
 
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
 
 client_credentials_manager = SpotifyClientCredentials(
-    client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    client_id=client_id, client_secret=client_secret)
 
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
